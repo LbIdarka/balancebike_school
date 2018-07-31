890187be5b3f6f3068d5efcdcdc1b41d96479611
Alena Nikolaenko <lbidarka@gmail.com>
2018-07-31 17:47:36 +0300

    оптимизация картинок, добавление webp

--- gulpfile.js
+++ gulpfile.js
@@ -4,77 +4,40 @@ var gulp = require("gulp");
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
-var server = require("browser-sync").create();
-var rename = require("gulp-rename");
-var minify = require("gulp-csso");
 var imagemin = require("gulp-imagemin");
 var webp = require("gulp-webp");
 var run = require("run-sequence");
-var del = require("del");
 
 gulp.task("style", function() {
-  gulp.src("css/style.css")
+  gulp.src("source/css/style.css")
     .pipe(plumber())
     .pipe(postcss([
       autoprefixer()
     ]))
-  .pipe(gulp.dest("build/css"))
-  .pipe(minify())
-  .pipe(rename("style.min.css"))
-  .pipe(gulp.dest("build/css"))
-  .pipe(server.stream());
+    .pipe(gulp.dest("source/css"))
 });
 
 gulp.task("images", function () {
-  return gulp.src("img/**/*.{png,jpg,svg}")
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
     .pipe(imagemin([
       imagemin.optipng({optimizationLevel: 3}),
       imagemin.jpegtran({progressive: true}),
       imagemin.svgo()
-    ],
-    {
-      verbose:true
-    }
-  ))
-    .pipe(gulp.dest("build/img"));
+    ]))
+    .pipe(gulp.dest("source/img"));
 });
 
 gulp.task("webp", function () {
-  return gulp.src("img/**/*.{png,jpg}")
+  return gulp.src("source/img/**/*.{png,jpg}")
     .pipe(webp({quality: 90}))
-    .pipe(gulp.dest("build/img/webp"));
-});
-
-gulp.task("clean", function () {
-  return del("build");
-});
-
-gulp.task("copy", function () {
-  return gulp.src([
-    "fonts/**/*.{woff,woff2}",
-    "img/**",
-    "js/**",
-    "*.html",
-    "slick/**"
-    ], {
-      base: "."
-    })
-    .pipe(gulp.dest("build"));
-});
-
-gulp.task("serve", function() {
-  server.init({
-    server: "build/",
-    notify: false,
-    open: true,
-    cors: true,
-    ui: false
-  });
-
-  gulp.watch("*.css", ["style"]);
-  gulp.watch("*.html").on("change", server.reload);
+    .pipe(gulp.dest("source/img/webp"));
 });
 
 gulp.task("build", function (done) {
-  run("clean", "copy", "style", "webp", "images", done);
+  run(
+    "style",
+    "images",
+    "webp",
+    done
+  );
 });
