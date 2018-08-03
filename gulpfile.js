99fdb8140aee2fca47a76e664926fa9024965472
Alena Nikolaenko <lbidarka@gmail.com>
2018-08-03 16:25:36 +0300

    добавила фавиконку

--- gulpfile.js
+++ gulpfile.js
@@ -22,6 +22,7 @@ gulp.task("copy", function() {
   return gulp.src([
       "source/fonts/**/*.{woff,woff2}",
       "source/img/**",
+      "source/favicon/**",
       "source/js/**",
       "source/slick/**"
     ], {
